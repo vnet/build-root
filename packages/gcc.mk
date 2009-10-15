5d460a243f36d0de963af5bad1802960d9bb3bd0
David Barach <dbarach@sw-oplin-1.cisco.com>
2009-10-15 15:15:59 -0400

    Reenable threads in libgcc.a, enable shared version of libgcc.  glibc has a sneaky dependency on libgcc which is used to make pthread_join() / pthread_exit() work.

--- packages/gcc.mk
+++ packages/gcc.mk
@@ -12,17 +12,10 @@ gcc_configure_args += --disable-libmudflap
 gcc_configure_args += --disable-libssp
 gcc_configure_args += --disable-libgomp
 
-# Don't want libgcc.a to be shared; this breaks cross-build
-gcc_configure_args += --disable-shared
-
 # Depends on GLIBC
 gcc_configure_args += --enable-decimal-float=no
 
 gcc_configure_args += --enable-languages=c
-
-# Disables threads in libgcc.a.  Otherwise we would depend on GLIBC
-# while compiling GCC (chicken & egg problem)
-gcc_configure_args += --enable-threads=single
 
 # Newer versions of GCC depend on MPFR/GMP libraries
 gcc_configure_args += \
