8657369d6cb7a69aeb409dcaf0206180098cd7dd
David Barach <dbarach@sw-oplin-1.cisco.com>
2009-10-16 07:24:30 -0400

    New file

---
+++ packages/gcc-bootstrap.mk
@@ -0,0 +1,66 @@
+gcc-bootstrap_source = gcc
+
+# GCC install depends on ranlib from binutils
+gcc-bootstrap_configure_depend = binutils-install
+
+gcc-bootstrap_configure_host_and_target = --target=$(TARGET)
+
+# gcc-bootstrap_make_parallel_fails = yes
+
+# Remove unneeded stuff
+gcc-bootstrap_configure_args = --disable-nls
+gcc-bootstrap_configure_args += --disable-multilib
+gcc-bootstrap_configure_args += --disable-libmudflap
+gcc-bootstrap_configure_args += --disable-libssp
+gcc-bootstrap_configure_args += --disable-libgomp
+
+# Don't want libgcc.a to be shared; this breaks cross-build
+gcc-bootstrap_configure_args += --disable-shared
+
+# Disables threads in libgcc.a.  Otherwise we would depend on GLIBC
+# while compiling GCC (chicken & egg problem)
+gcc-bootstrap_configure_args += --enable-threads=single
+
+# Depends on GLIBC
+gcc-bootstrap_configure_args += --enable-decimal-float=no
+
+gcc-bootstrap_configure_args += --enable-languages=c
+
+# Newer versions of GCC depend on MPFR/GMP libraries
+gcc-bootstrap_configure_args += \
+  --with-mpfr-include=$(call installed_include_fn,mpfr) \
+  --with-mpfr-lib=$(TOOL_INSTALL_DIR)/lib$(native_libdir)
+gcc-bootstrap_configure_args += \
+  --with-gmp-include=$(call installed_include_fn,gmp) \
+  --with-gmp-lib=$(TOOL_INSTALL_DIR)/lib$(native_libdir)
+
+# Could put $(ARCH) dependent flags here
+# For example, if $(ARCH)=foo
+# gcc-bootstrap_configure_args_foo = --enable-foo-bar
+
+gcc-bootstrap_configure_args_armiwmmxt = --with-arch=iwmmxt --with-abi=iwmmxt
+
+# Architecture dependent configure flags
+gcc-bootstrap_configure_args += $(gcc-bootstrap_configure_args_$(ARCH))
+
+# Platform dependent configure flags
+gcc-bootstrap_configure_args += $(gcc-bootstrap_configure_args_$(PLATFORM))
+
+# We need LIMITS_H_TEST=true to appease gcc/gcc/Makefile.in
+# Otherwise gcc-lib/include/limits.h will be missing #include_next <limits.h>
+# to pick up the linux's limits.h
+gcc-bootstrap_make_args += LIMITS_H_TEST=true
+
+gcc-bootstrap_make_args += \
+  LIBGCC2_INCLUDES="-idirafter $(PACKAGE_BUILD_DIR)/limits_h_kludge"
+
+gcc-bootstrap_build = \
+  mkdir -p $(PACKAGE_BUILD_DIR)/limits_h_kludge ; \
+  touch $(PACKAGE_BUILD_DIR)/limits_h_kludge/limits.h ; \
+  $(PACKAGE_MAKE) ; \
+  rm -rf $(PACKAGE_BUILD_DIR)/limits_h_kludge
+
+ # Depends on GLIBC
+ gcc-bootstrap_configure_args += --enable-decimal-float=no
+ 
+ gcc-bootstrap_configure_args += --enable-languages=c
