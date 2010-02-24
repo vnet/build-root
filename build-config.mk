56c712c2fbcecabccffe6332d6364d1894a9f18c
N Srinivasan <nsriniva@ludd-dev-01.cisco.com>
2010-02-23 22:17:31 -0800

    Minor mods.

--- build-config.mk
+++ build-config.mk
@@ -1,3 +1,4 @@
 # To specify directories to find sources, build/packages/*.mk
 # and build/platforms.mk
 # SOURCE_PATH = PATH1 PATH2 ...
+SOURCE_PATH = /scratch/nsriniva/q-lnx/open-repo /scratch/nsriniva/q-lnx/closed-repo
