6a94d541aa3ad6b249e56d708170181d0de7ae2c
David Barach <dbarach@sw-eclipse-14.cisco.com>
2010-02-24 06:10:49 -0500

    The checked-in version of this file should not set SOURCE_PATH, especially not to /scratch/joecisco/<yadda>

--- build-config.mk
+++ build-config.mk
@@ -1,4 +1,3 @@
 # To specify directories to find sources, build/packages/*.mk
 # and build/platforms.mk
 # SOURCE_PATH = PATH1 PATH2 ...
-SOURCE_PATH = /scratch/nsriniva/q-lnx/open-repo /scratch/nsriniva/q-lnx/closed-repo
