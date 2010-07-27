0873e547e244733c42205b0d42a6fa23bea11b3c
Burt Silverman <bsilverm@cisco.com>
2010-07-27 10:26:12 -0700

    use configure script, to remove generated files.

--- packages/automake.mk
+++ packages/automake.mk
@@ -1,1 +1,10 @@
-automake_configure_depend = autoconf-install
+automake_configure =						\
+  s=$(call find_source_fn,$(PACKAGE_SOURCE)) ;                  \
+  cd $$s ;							\
+  $$s/bootstrap ;						\
+  if [ ! -f $$s/configure ] ; then                              \
+    autoreconf -i -f $$s ;                                      \
+  fi ;                                                          \
+  cd $(PACKAGE_BUILD_DIR) ;                                     \
+  env $(CONFIGURE_ENV)                                          \
+    $$s/configure --prefix=$(PACKAGE_INSTALL_DIR)
